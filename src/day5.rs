751bb3ff9e4e74b8f54c86d26394a2782690822a
Dr. Michelle Fernandez Bieber <mfbieber@gmail.com>
2023-12-10 17:42:24 +0100

    Day 5 is correct for part 1

--- src/day5.rs
+++ src/day5.rs
@@ -1,4 +1,6 @@
+use std::ops::{Deref, DerefMut};
 use std::path::PathBuf;
+use crate::day3::Number;
 use crate::read_lines;
 
 fn day5_part2(path: &PathBuf) -> u32 {
@@ -15,141 +17,140 @@ fn day5_part2(path: &PathBuf) -> u32 {
 
 const SIZE: usize = 11322857500;
 
-fn day5_part1(path: &PathBuf, seeds: Vec<i64>) -> i64 {
-    let mut sum: i32 = 0;
+#[derive(Clone)]
+struct MappingRange {
+    source: i64,
+    dest: i64,
+    range: i64
+}
 
-    let mut seed_to_soil: Vec<i64> = vec![0; SIZE];
-    let mut soil_to_fertilizer: Vec<i64> = vec![0; SIZE];
-    let mut fertilizer_to_water: Vec<i64> = vec![0; SIZE];
-    let mut water_to_light: Vec<i64> = vec![0; SIZE];
-    let mut light_to_temperature: Vec<i64> = vec![0; SIZE];
-    let mut temperature_to_humidity: Vec<i64> = vec![0; SIZE];
-    let mut humidity_to_location: Vec<i64> = vec![0; SIZE];
+impl MappingRange {
+
+    fn get_dest(&self, value: i64) -> i64 {
+        let diff: i64 = self.source - self.dest;
+        return value - diff;
+    }
+
+    fn is_in_dest_range(&self, value: i64) -> bool {
+        return (self.dest + self.range) >= value && (self.dest <= value)
+    }
+
+    fn is_in_source_range(&self, value: i64) -> bool {
+        return (self.source + self.range) >= value && (self.source <= value)
+    }
+}
+
+#[derive(Clone)]
+struct Mapping {
+    name: String,
+    mapping_ranges: Vec<MappingRange>
+}
+
+impl Mapping {
+    fn push_mapping_range(&mut self, range: MappingRange) {
+        let owned_range = range;
+        self.mapping_ranges.push(owned_range);
+    }
+}
+
+fn day5_part1(path: &PathBuf, seeds: Vec<i64>) -> i64 {
+
+    let mut mappings: Vec<Mapping> = vec![];
+    let mut mapping_name = String::from("");
+    let mut mr0: Vec<MappingRange> = vec![];
+    let mut mr1: Vec<MappingRange> = vec![];
+    let mut mr2: Vec<MappingRange> = vec![];
+    let mut mr3: Vec<MappingRange> = vec![];
+    let mut mr4: Vec<MappingRange> = vec![];
+    let mut mr5: Vec<MappingRange> = vec![];
+    let mut mr6: Vec<MappingRange> = vec![];
 
     // File input.txt must exist in the current path
     if let Ok(lines) = read_lines(path) {
-        let mut mapping = String::from("");
         for line in lines {
             let line_clone = line.unwrap().clone();
-            if line_clone.ends_with("map:") {
-                mapping = line_clone;
-                continue;
-            }
             if line_clone.len() > 0 {
-                do_mapping(&mut seed_to_soil, &mut soil_to_fertilizer, &mut fertilizer_to_water,
-                           &mut water_to_light, &mut light_to_temperature, &mut temperature_to_humidity,
-                           &mut humidity_to_location, &line_clone, &mapping);
+                if line_clone.ends_with("map:") {
+                    mapping_name = line_clone;
+                    continue;
+                }
+                let mapping_range: MappingRange = get_mapping_range(&line_clone, &mapping_name);
+                if mapping_name.starts_with("seed-to-soil") {
+                    mr0.push(mapping_range);
+                } else if mapping_name.starts_with("soil-to-fertilizer") {
+                    mr1.push(mapping_range);
+                } else if mapping_name.starts_with("fertilizer-to-water") {
+                    mr2.push(mapping_range);
+                } else if mapping_name.starts_with("water-to-light") {
+                    mr3.push(mapping_range);
+                } else if mapping_name.starts_with("light-to-temperature") {
+                    mr4.push(mapping_range);
+                } else if mapping_name.starts_with("temperature-to-humidity") {
+                    mr5.push(mapping_range);
+                } else if mapping_name.starts_with("humidity-to-location") {
+                    mr6.push(mapping_range);
+                }
             }
-            println!("{}", mapping);
         }
     }
+    mappings.push(Mapping {
+        name: String::from("seed-to-soil"),
+        mapping_ranges: mr0,
+    });
+    mappings.push(Mapping {
+        name: String::from("soil-to-fertilizer"),
+        mapping_ranges: mr1,
+    });
+    mappings.push(Mapping {
+        name: String::from("fertilizer-to-water"),
+        mapping_ranges: mr2,
+    });
+    mappings.push(Mapping {
+        name: String::from("water-to-light"),
+        mapping_ranges: mr3,
+    });
+    mappings.push(Mapping {
+        name: String::from("light-to-temperature"),
+        mapping_ranges: mr4,
+    });
+    mappings.push(Mapping {
+        name: String::from("temperature-to-humidity"),
+        mapping_ranges: mr5,
+    });
+    mappings.push(Mapping {
+        name: String::from("humidity-to-location"),
+        mapping_ranges: mr6,
+    });
+    println!("mappings");
 
     let mut smallest_location: i64 = SIZE as i64;
     for seed in seeds {
-        let mut soil: i64 = seed_to_soil[seed as usize];
-        if soil == 0 {
-            soil = seed;
+        let mut dest: i64 = seed;
+        for mapping in &mappings {
+            for range in &mapping.mapping_ranges {
+                if range.is_in_source_range(dest) {
+                    dest = range.get_dest(dest);
+                    break;
+                }
+            }
         }
-        let mut fertilizer: i64 = soil_to_fertilizer[soil as usize];
-        if fertilizer == 0 {
-            fertilizer = soil;
-        }
-        let mut water: i64 = fertilizer_to_water[fertilizer as usize];
-        if water == 0 {
-            water = fertilizer;
-        }
-        let mut light: i64 = water_to_light[water as usize];
-        if light == 0 {
-            light = water;
-        }
-        let mut temperature: i64 = light_to_temperature[light as usize];
-        if temperature == 0 {
-            temperature = light;
-        }
-        let mut humidity: i64 = temperature_to_humidity[temperature as usize];
-        if humidity == 0 {
-            humidity = temperature;
-        }
-        let mut location: i64 = humidity_to_location[humidity as usize];
-        if location == 0 {
-            location = humidity;
-        }
-        if location < smallest_location {
-            smallest_location = location;
+        if dest < smallest_location {
+            smallest_location = dest;
         }
     }
-
     println!("{}", smallest_location);
     return smallest_location;
 }
 
-fn do_mapping(seed_to_soil: &mut Vec<i64>, soil_to_fertilizer: &mut Vec<i64>,
-              fertilizer_to_water: &mut Vec<i64>, water_to_light: &mut Vec<i64>,
-              light_to_temperature: &mut Vec<i64>, temperature_to_humidity: &mut Vec<i64>,
-              humidity_to_location: &mut Vec<i64>, line_ref: &String, mut mapping: &String) {
+fn get_mapping_range(line_ref: &String, mut mapping_name: &String) -> MappingRange {
     let split_line: Vec<i64> = line_ref.split(" ")
         .map(|item| item.parse().unwrap())
         .collect();
-    let mut source_range_start: i64 = split_line[1];
-    let mut dest_range_start: i64 = split_line[0];
-    let range_length: i64 = split_line[2];
-    if mapping.starts_with("seed-to-soil") {
-        let mut i : i64 = 0;
-        while i < range_length {
-            seed_to_soil[source_range_start as usize] = dest_range_start;
-            dest_range_start += 1;
-            source_range_start += 1;
-            i += 1;
-        }
-    } else if mapping.starts_with("soil-to-fertilizer") {
-        let mut i : i64 = 0;
-        while i < range_length {
-            soil_to_fertilizer[source_range_start as usize] = dest_range_start;
-            dest_range_start += 1;
-            source_range_start += 1;
-            i += 1;
-        }
-    } else if mapping.starts_with("fertilizer-to-water") {
-        let mut i : i64 = 0;
-        while i < range_length {
-            fertilizer_to_water[source_range_start as usize] = dest_range_start;
-            dest_range_start += 1;
-            source_range_start += 1;
-            i += 1;
-        }
-    } else if mapping.starts_with("water-to-light") {
-        let mut i : i64 = 0;
-        while i < range_length {
-            water_to_light[source_range_start as usize] = dest_range_start;
-            dest_range_start += 1;
-            source_range_start += 1;
-            i += 1;
-        }
-    } else if mapping.starts_with("light-to-temperature") {
-        let mut i : i64 = 0;
-        while i < range_length {
-            light_to_temperature[source_range_start as usize] = dest_range_start;
-            dest_range_start += 1;
-            source_range_start += 1;
-            i += 1;
-        }
-    } else if mapping.starts_with("temperature-to-humidity") {
-        let mut i : i64 = 0;
-        while i < range_length {
-            temperature_to_humidity[source_range_start as usize] = dest_range_start;
-            dest_range_start += 1;
-            source_range_start += 1;
-            i += 1;
-        }
-    } else if mapping.starts_with("humidity-to-location") {
-        let mut i : i64 = 0;
-        while i < range_length {
-            humidity_to_location[source_range_start as usize] = dest_range_start;
-            dest_range_start += 1;
-            source_range_start += 1;
-            i += 1;
-        }
-    }
+    return MappingRange {
+        source: split_line[1],
+        dest: split_line[0],
+        range: split_line[2],
+    };
 }
 
 //destination range start, the source range start, and the range length
@@ -157,7 +158,47 @@ fn do_mapping(seed_to_soil: &mut Vec<i64>, soil_to_fertilizer: &mut Vec<i64>,
 #[cfg(test)]
 mod tests {
     use std::path::PathBuf;
-    use crate::day5::day5_part1;
+    use crate::day5::{day5_part1, MappingRange};
+
+    #[test]
+    fn test_is_in_source_range() {
+        let mapping_range: MappingRange = MappingRange {
+            source: 0,
+            dest: 6,
+            range: 5,
+        };
+        assert_eq!(mapping_range.is_in_source_range(2), true);
+    }
+
+    #[test]
+    fn test_is_in_dest_range() {
+        let mapping_range: MappingRange = MappingRange {
+            source: 0,
+            dest: 6,
+            range: 5,
+        };
+        assert_eq!(mapping_range.is_in_dest_range(2), false);
+    }
+
+    #[test]
+    fn test_get_dest() {
+        let mapping_range: MappingRange = MappingRange {
+            source: 0,
+            dest: 6,
+            range: 5,
+        };
+        assert_eq!(mapping_range.get_dest(2), 8);
+    }
+
+    #[test]
+    fn test_get_dest2() {
+        let mapping_range: MappingRange = MappingRange {
+            source: 6,
+            dest: 0,
+            range: 5,
+        };
+        assert_eq!(mapping_range.get_dest(8), 2);
+    }
 
     #[test]
     fn test_part1() {
@@ -174,7 +215,7 @@ mod tests {
         let seeds: Vec<i64> = vec![5844012, 110899473, 1132285750, 58870036, 986162929, 109080640,
             3089574276, 100113624, 2693179996, 275745330, 2090752257, 201704169, 502075018,
             396653347, 1540050181, 277513792, 1921754120, 26668991, 3836386950, 66795009];
-        assert_eq!(day5_part1(&d, seeds), 35);
+        assert_eq!(day5_part1(&d, seeds), 825516882);
     }
 
     #[test]
